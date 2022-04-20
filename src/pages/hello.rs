84b11dde71075d34a9eac765ebc91b4cbca94d43
Nick Chow <nicky.chow@gmail.com>
2022-04-20 12:58:25 +1000

    dump dependencies

--- src/pages/hello.rs
+++ src/pages/hello.rs
@@ -1,12 +1,22 @@
+// use gloo::console::log;
 use yew::prelude::*;
+use yew_router::prelude::*;
+
+use crate::Route;
 
 use crate::components::molecules::HelloForm;
 
 #[function_component(Hello)]
 pub fn hello() -> Html {
+    let history = use_history().unwrap();
+    let onclick = Callback::from(move |_| {
+        history.push(Route::Home);
+    });
     html! {
         <>
             <HelloForm />
+
+            <button {onclick}>{"Go Home"}</button>
         </>
     }
 }
